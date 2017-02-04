ab4653e4f5a0e232711ea401a9aa0ceb226337b2
soggysquid <susan_mendel@me.com>
2017-02-03 21:40:53 -0700

    Added in bin plot to make_bias_plot

--- func_make_bias_plot.m
+++ func_make_bias_plot.m
@@ -1,18 +1,32 @@
-function [ Xper, W ] = func_make_bias_plot( x, w, k, Nfft,doplot,Ewin)
+function [ Xper, W ] = func_make_bias_plot( x, w, k, Nfft, type, Ewin)
+doplot = 1;
 w=[zeros(Nfft/2*(Nfft-1),1);w;zeros(Nfft/2*(Nfft-1)-1,1)]; %zero-pad
 Xper = zeros(Nfft^2,1);
 Xper(1:Nfft:length(Xper)) = (fft(x).*conj(fft(x)))/Nfft^2;
 Xper = Xper/Ewin;
+Xmax = 10*log10(max(Xper))
 % w_fft = 0.5*fft(w,Nfft*Nfft)/Nfft;
 w_fft = fft(w,Nfft*Nfft)/Nfft;
-w_fft = w_fft.*w_fft*0.5/Ewin;
-w_fft1 = circshift(w_fft, Nfft*Nfft/2);
-w_fft2 = circshift(w_fft, Nfft*(Nfft/2+1));
-W=w_fft1+w_fft2;
+if type
+    w_fft = w_fft.*w_fft*0.5/Ewin;
+    w_fft1 = circshift(w_fft, Nfft*Nfft/2);
+    w_fft2 = circshift(w_fft, Nfft*(Nfft/2+1));
+    W=w_fft1+w_fft2;
+else
+    w_fft = w_fft.*w_fft/Ewin;
+    W = circshift(w_fft, Nfft*Nfft/2);
+end
+
+unity = ones(length(Xper),1);
 if doplot
     scatter([0:length(Xper)-1]/Nfft,10*log10(Xper));
     hold('on')
     plot([0:length(Xper)-1]/Nfft,10*log10(abs(W)))
+    plot([0:length(Xper)-1]/Nfft,Xmax*unity,'--')
+    x1 = 3;
+    y1 = Xmax+2;
+    txt = sprintf('%0.2f dB',Xmax);
+    text(x1,y1,txt)
     xlim([0 Nfft]);
     ylim([-100 5]);
     ylabel('dB')
--- make_bias_plot_hann.m
+++ make_bias_plot_hann.m
@@ -8,15 +8,24 @@ xmin=0;
 deltaf=0;
 dir='.';
 [x,findex,fs] = create_test_vector(source, Nfft, L, sigma, xmin, deltaf, dir);
+source = 4;
+[x1,findex,fs] = create_test_vector(source, Nfft, L, sigma, xmin, deltaf, dir);
 k=[-Nfft/2:Nfft/2];
 k=k(1:end-1)';
 wb=[ones(Nfft,1)].*(Nfft-abs(k))/Nfft;
 wr=[ones(Nfft,1)];
-wh=hann(Nfft+1);
+wh=blackmanharris(Nfft+1);
 wh=wh(1:end-1);
 xh=wh.*x;
+xh1=wh.*x1;
 figure(1)
-[Xper,Wb] = func_make_bias_plot(x,wr,k,Nfft,1,1);
+Ewin = 1
+[Xper,Wb] = func_make_bias_plot(x,wr,k,Nfft,1,Ewin);
 figure(2)
+[Xper,Wb] = func_make_bias_plot(x1,wr,k,Nfft,0,Ewin);
+figure(3)
 Ewin = sum(wh.^2)/Nfft;
 [Xperh,Wh] = func_make_bias_plot(xh,wh,k,Nfft,1,Ewin);
+figure(4)
+[Xperh,Wh] = func_make_bias_plot(xh1,wh,k,Nfft,0,Ewin);
+
